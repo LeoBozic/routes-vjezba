4892d8e1eb0bf93f381df4b240dbc8390a6fee16
Leo Božić <leobozic2903@gmail.com>
2023-07-24 20:29:02 +0200

    gotovo

---
+++ src/pages/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <div>Ovo je stranica o nama</div>;
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div>Početna stranica</div>;
+}
+
+export default Home;
